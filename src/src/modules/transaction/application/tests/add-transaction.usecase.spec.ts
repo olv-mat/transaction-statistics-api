8466f05e76fa5b1bce621f3e7b7f2d0b71accfdc
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-06-21 08:01:57 -0300

    test: add execute tests for add transaction use case

---
+++ src/src/modules/transaction/application/tests/add-transaction.usecase.spec.ts
@@ -0,0 +1,49 @@
+import { UnprocessableEntityException } from '@nestjs/common';
+import 'reflect-metadata';
+import { makeAddTransactionDto } from '../../presentation/tests/factories/add-transaction.dto.factory';
+import { AddTransactionUseCase } from '../use-cases/add-transaction.usecase';
+
+describe('AddTransactionUseCase', () => {
+  let addTransactionUseCase: AddTransactionUseCase;
+  const transactionRepositoryMock = {
+    save: jest.fn(),
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    addTransactionUseCase = new AddTransactionUseCase(
+      transactionRepositoryMock,
+    );
+  });
+
+  describe('execute', () => {
+    it('should add a new transaction', () => {
+      const dto = makeAddTransactionDto();
+      addTransactionUseCase.execute(dto);
+      expect(transactionRepositoryMock.save).toHaveBeenCalledWith(
+        expect.objectContaining({
+          amount: dto.amount,
+          timestamp: dto.timestamp,
+        }),
+      );
+    });
+
+    it('should fail if timestramp is in the future', () => {
+      const dto = makeAddTransactionDto({
+        timestamp: new Date(Date.now() + 60000),
+      });
+      expect(() => addTransactionUseCase.execute(dto)).toThrow(
+        UnprocessableEntityException,
+      );
+    });
+
+    it('should fail if amount is negative', () => {
+      const dto = makeAddTransactionDto({
+        amount: -123.45,
+      });
+      expect(() => addTransactionUseCase.execute(dto)).toThrow(
+        UnprocessableEntityException,
+      );
+    });
+  });
+});
