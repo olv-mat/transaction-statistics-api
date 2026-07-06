3e963e0e9f138e89f182804863e417f1a962830d
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-07-06 06:14:51 -0300

    chore: add helmet for security headers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 import { TransactionModule } from './src/modules/transaction/transaction.module';
 
 @Module({
-  imports: [TransactionModule, LoggerModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule.forRoot(),
+    TransactionModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { Environments } from './src/shared/enums/environments.enum';
 import { swaggerSetup } from './src/shared/settings/swagger/swagger.setup';
 
 async function bootstrap() {
@@ -13,6 +15,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  if (process.env.NODE_ENV === Environments.PRODUCTION) {
+    app.use(helmet());
+    app.enableCors({});
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 
