0379ed7428c6162a75f6222a3b5cefe48bb46e2e
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-07-09 07:05:17 -0300

    docs: document health check endpoint

--- src/src/modules/health/presentation/health.controller.ts
+++ src/src/modules/health/presentation/health.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  SwaggerInternalServerError,
+  SwaggerOperation,
+} from 'src/src/shared/settings/swagger/swagger.decorators';
 import { HealthResponseDto } from './dtos/health-response.dto';
 
 @Controller('health')
 export class HealthController {
   @Get()
+  @SwaggerOperation('Check application health')
+  @SwaggerInternalServerError()
   public health(): HealthResponseDto {
     return HealthResponseDto.create({
       status: 'ok',
